5037f5842b1f0a7f784e989ebd37569ea2bff81f
UwU <uwu@jc.jc>
2023-06-11 14:23:45 +0300

    lua: telescope buffers in normal mode

--- lua/plugins/telescope-conf.lua
+++ lua/plugins/telescope-conf.lua
@@ -2,13 +2,14 @@ local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 
-vim.keymap.set('n', '<leader>d', builtin.find_files, {})
 vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>a', builtin.buffers, {})
+vim.keymap.set('n', '<leader>d', builtin.find_files, {})
 vim.keymap.set('n', '<leader>s', builtin.help_tags, {})
+-- vim.keymap.set('n', '<leader>a', builtin.buffers, {})
 
-vim.api.nvim_set_keymap('n', '<space>e', ':Telescope file_browser initial_mode=normal <CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<space>a', ':lua require"telescope.builtin".find_files{ on_complete = { function() vim.cmd"stopinsert" end } }<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<space>r', ':Telescope file_browser initial_mode=normal path=%:p:h select_buffer=true<CR>', { noremap = true })
+-- vim.api.nvim_set_keymap('n', '<space>e', ':Telescope file_browser initial_mode=normal <CR>', { noremap = true })
 
 telescope.setup {
   mappings = {
